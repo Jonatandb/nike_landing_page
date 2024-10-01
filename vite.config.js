6935817ebfbebe89db097007f50fb2c4c9d4e204
Jonatandb <jonatandb@gmail.com>
2024-10-01 19:02:52 -0300

    Github pages configuration + production build (WIP)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === "development" ? "/" : "/nike_landing_page/",
+  build: {
+    outDir: "docs",
+  }
+}))
