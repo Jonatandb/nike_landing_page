4b75a1868fb6dca2cd2296c3b9fa5cd515123713
Jonatandb <jonatandb@gmail.com>
2024-10-03 17:48:20 -0300

    SpecialOffer section added

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,9 +2,9 @@ import Hero from "./Hero";
 import PopularProducts from "./PopularProducts";
 import SuperQuality from "./SuperQuality";
 import Services from "./Services";
-import SpecialOffers from "./SpecialOffers";
+import SpecialOffer from "./SpecialOffer";
 import CustomerReviews from "./CustomerReviews";
 import Subscribe from "./Subscribe";
 import Footer from "./Footer";
 
-export { Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer }
+export { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer }
