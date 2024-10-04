b51a296b0aa0ff233f59ed8986d94a160795b688
Jonatandb <jonatandb@gmail.com>
2024-10-04 15:50:12 -0300

    CSS Animations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import Navbar from './components/Navbar'
 import {
   Hero,
@@ -11,31 +13,51 @@ import {
 } from './sections'
 
 const App = () => {
+  useEffect(() => {
+    function animateScroll() {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible')
+          }
+        })
+      })
+
+      document
+        .querySelectorAll('.animateFromLeft, .animateFromRight, .animate')
+        .forEach(section => {
+          observer.observe(section)
+        })
+    }
+
+    animateScroll()
+  }, [])
+
   return (
     <main className='relative'>
       <Navbar />
-      <section className='xl:padding-l wide:padding-r padding-b'>
+      <section className='animate xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className='padding'>
+      <section className='animateFromRight padding'>
         <PopularProducts />
       </section>
-      <section className='padding'>
+      <section className='animateFromLeft padding'>
         <SuperQuality />
       </section>
-      <section className='padding-x py-10'>
+      <section className='animateFromRight padding-x py-10'>
         <Services />
       </section>
-      <section className='padding'>
+      <section className='animateFromLeft padding'>
         <SpecialOffer />
       </section>
-      <section className='padding bg-pale-blue'>
+      <section className='animateFromRight padding bg-pale-blue'>
         <CustomerReviews />
       </section>
-      <section className='padding-x sm:py-32 py-16 w-full'>
+      <section className='animateFromLeft padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className='padding-x padding-t bg-black pb-8'>
+      <section className='animate padding-x padding-t bg-black pb-8'>
         <Footer />
       </section>
     </main>
